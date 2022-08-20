ba7897da436cd0318c1136201f668b56754c5468
jart2 <jart2607@gmail.com>
2022-08-19 23:25:17 -0500

    Clase Observer terminado Nota: la funcion lamda en addObserver reconoce la funcion que hace referencia por el numero de argumentos

---
+++ src/org/aguzman/patrones/observer/ejemplo/EjemploObserver.java
@@ -0,0 +1,23 @@
+package org.aguzman.patrones.observer.ejemplo;
+
+import org.aguzman.patrones.observer.Corporacion;
+
+public class EjemploObserver {
+    public static void main(String[] args) {
+        Corporacion google=new Corporacion("Google",1000);
+        google.addObserver((observable, obj) ->  {
+            System.out.println("John: "+observable);
+        });
+
+        google.addObserver((observable, obj)  -> {
+            System.out.println("Andres: "+observable);
+        });
+
+        google.addObserver((observable, obj)  -> {
+            System.out.println("Maria: "+observable);
+        });
+
+
+        google.modificaPrecio(2000);
+    }
+}
